20ce48be658aecbe9c1299fe43a09cdee4384320
Erik200031 <90761494+Erik200031@users.noreply.github.com>
2021-10-25 21:26:20 +0400

    Update temperature.h

--- temperature.h
+++ temperature.h
@@ -7,19 +7,17 @@
 class Temperature 
 {
   public:
-  Temperature();
-  void menu();
-  void enter_c_f_k();
-  void write_data();
-  void getTimeStr();
-  void change_date_format();
-  void start_auto_writting();
+    Temperature();
+    void menu();
+    void enter_c_f_k();
+    void write_data();
+    void getTimeStr();
+    void change_date_format();
+    void start_auto_writting();
   private:
     std::string tmp,tmp01,tmp02,result;
     double celsius;
     char unit;
-
 };
-
 
 #endif //TEMPERATURE_H_
